06c58c4536daac06ac7fed5e8e6a19e204ff1846
Simon Sassi <2765875+maxerbox@users.noreply.github.com>
2023-01-16 13:54:04 +0100

    feat: login implem
    Co-authored-by: Maël <mlhoutel@users.noreply.github.com>

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,19 +1,29 @@
+from pathlib import Path
+
 import environ
 import os
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent.parent.parent
+
 
 env = environ.Env(
     NAOMESH_ORCHESTRATOR_DEBUG=(bool, False),
     NAOMESH_ORCHESTRATOR_GRID5000_USERNAME=str,
     NAOMESH_ORCHESTRATOR_GRID5000_PASSWORD=str,
-    NAOMESH_ORCHESTRATOR_GRID5000_VERIFY_SSL=str,
-    NAOMESH_ORCHESTRATOR_GRID5000_TARGET_SITE=str
+    NAOMESH_ORCHESTRATOR_GRID5000_VERIFY_SSL=(bool, False),
+    NAOMESH_ORCHESTRATOR_GRID5000_TIMEOUT=(float, None),
+    NAOMESH_ORCHESTRATOR_GRID5000_SSLCERT=(str, None),
+    NAOMESH_ORCHESTRATOR_GRID5000_SSLKEY=(str, None),
+    NAOMESH_ORCHESTRATOR_GRID5000_SSLUSER=(str, "anonymous"),
+    NAOMESH_ORCHESTRATOR_GRID5000_TARGET_SITE=str,
+    NAOMESH_ORCHESTRATOR_DOCKER_USERNAME=(str, None),
+    NAOMESH_ORCHESTRATOR_DOCKER_PASSWORD=(str, None),
 )
 
 # Set the project base directory
-BASE_DIR = os.path.abspath(os.path.join(__file__ ,"../../../.."))
+BASE_DIR = os.path.abspath(get_project_root())
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-
-
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
