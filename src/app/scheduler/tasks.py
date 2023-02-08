07fdb92439c0548ed9fb78a98080b1d55d4f1335
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2023-02-08 09:03:14 +0100

    refactor: move task_input_hash to utils

--- src/app/scheduler/tasks.py
+++ src/app/scheduler/tasks.py
@@ -1,49 +1,15 @@
 from datetime import timedelta
 from os import path
-from typing import Any, Dict, Optional
 
 import enoslib as en
 from enoslib import Roles
 from prefect import task
-from prefect.context import TaskRunContext
-from prefect.utilities.hashing import hash_objects
 
 from app.core.config import BASE_DIR, env
-from app.scheduler.utils import with_redirect_stdout_to_run_logger
-
-
-def task_input_hash_no_roles(
-    context: "TaskRunContext", arguments: Dict[str, Any]
-) -> Optional[str]:
-    """
-    A task cache key implementation which hashes all inputs
-    to the task using a JSON or
-    cloudpickle serializer. If any arguments are not JSON
-    serializable, the pickle
-    serializer is used as a fallback. If cloudpickle fails,
-    this will return a null key
-    indicating that a cache key could not be generated for the given inputs.
-
-    Arguments:
-        context: the active `TaskRunContext`
-        arguments: a dictionary of arguments to be passed to
-        the underlying task
-
-    Returns:
-        a string hash if hashing succeeded, else `None`
-    """
-    return hash_objects(
-        # We use the task key to get the qualified name for the task and
-        # include the task functions `co_code` bytes to avoid caching
-        # when the underlying function changes
-        context.task.task_key,
-        context.task.fn.__code__.co_code.hex(),
-        without_keys(arguments, "roles"),
-    )
-
-
-def without_keys(d, keys):
-    return {k: d[k] for k in d.keys() - keys}
+from app.scheduler.utils import (
+    task_input_hash_no_roles,
+    with_redirect_stdout_to_run_logger,
+)
 
 
 @task(
