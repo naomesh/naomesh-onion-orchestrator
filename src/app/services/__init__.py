4ec3ba6341252f9c231810846dfb698e317e05bc
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2023-02-22 16:51:09 +0100

    feat: implement current_allocated_nodes

--- src/app/services/__init__.py
+++ src/app/services/__init__.py
@@ -2,10 +2,10 @@ import os
 
 from app.services.amqp import AmqpTransport  # noqa: F401
 import app.services.jobs as jobs
-import app.services.currentallocatednodes as currentallocatednodes
+import app.services.current_allocated_nodes as current_allocated_nodes
 
 # A list of tomodachi services to start
 SERVICES_TO_RUN = [
-    jobs.__file__.replace(os.getcwd(), ""),
-    currentallocatednodes.__file__.replace(os.getcwd(), ""),
+    jobs.__file__.replace(os.getcwd(), "").strip("/"),
+    current_allocated_nodes.__file__.replace(os.getcwd(), "").strip("/"),
 ]
