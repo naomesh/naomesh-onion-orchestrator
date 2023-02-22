5d7e018f46b9eb7162913e666c0f50090cb8a117
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2023-02-22 21:39:53 +0100

    fix: replace first instead for service path

--- src/app/services/__init__.py
+++ src/app/services/__init__.py
@@ -5,7 +5,9 @@ import app.services.jobs as jobs
 import app.services.current_allocated_nodes as current_allocated_nodes
 
 # A list of tomodachi services to start
+# get relative paths from the current working directory, and strip the leading
+# slash
 SERVICES_TO_RUN = [
-    jobs.__file__,
-    current_allocated_nodes.__file__,
+    jobs.__file__.replace(os.getcwd(), "", 1).strip("/"),
+    current_allocated_nodes.__file__.replace(os.getcwd(), "", 1).strip("/"),
 ]
